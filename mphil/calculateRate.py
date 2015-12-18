23f51e7df4ce31ccb1e76a16fa1e8aaef1b987a2
hammad haleem <hammadhaleem@gmail.com>
2015-12-18 09:34:45 +0800

    updates

--- mphil/calculateRate.py
+++ mphil/calculateRate.py
@@ -1,6 +1,10 @@
 import json
+
 time_line = json.loads(open("dump.json").read())
-timer =  time_line.keys()
+timer =[]
+
+for i in time_line.keys():
+	timer.append(float(i))
 
 ta = 0 
 tb = 0.0 
@@ -13,21 +17,26 @@ n_t_a = 0.0
 
 r_a = []
 r_b = []
+
 for item in sorted(timer):
-	elements = time_line[item]
-	tb = float(item)
+	elements = time_line[str(item)]
+	tb = float(item) / 60.0
 	for obj in elements:
 		if obj['parentId'] == "-1": 
 			l_t_b = l_t_b + 1.0
 		else:
 			n_t_b = n_t_b + 1.0
 
-	r_a.append( (l_t_a - l_t_b) / (ta - tb))
-	r_b.append( (n_t_a - n_t_b) / (ta - tb))
+	r_a.append( [tb, (l_t_a - l_t_b) / (ta - tb)])
+	r_b.append( [tb, (n_t_a - n_t_b) / (ta - tb)])
 
-	ta = tb 
+	ta = tb
 	l_t_a = l_t_b
 	n_t_a = n_t_b
 
+stri = ""
 for i in range(0, len(timer)):
-	print r_a[i] , r_b[i] 
+	v1, v2 =r_b[i]
+	stri = stri + str(v1) + "," + str(v2) +"\n"
+
+open("out.csv","w+").write(stri)
