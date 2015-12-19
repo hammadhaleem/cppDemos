50e2c2b43d3155b98a0414d1674a585380f2f27d
hammad haleem <hammadhaleem@gmail.com>
2015-12-19 14:57:41 +0800

    r mean

--- mphil/Algorithm.py
+++ mphil/Algorithm.py
@@ -5,7 +5,7 @@ import json
 MAX_TWEETS  = 10**5 * 1.0
 time_max = 100.0
 percent = MAX_TWEETS / time_max
-
+recent = 5
 #loading things 
 time_line = json.loads(open("dump.json").read())
 N = 2*(10**5) # popularity traget
@@ -16,7 +16,7 @@ for i in time_line.keys():
 keys = sorted(keys)
 
 next = 0
-i = 0
+i_counter = 0
 
 
 ta = 0 
@@ -28,13 +28,14 @@ l_t_a = 0.0
 n_t_b = 0.0
 n_t_a = 0.0
 
-r_a = []
-r_b = []
+r_n = []
+r_e = []
 
-
+mean =[]
+mean_recent = []
 
 record = ""
-while i < 100 : 
+while i_counter < 100 : 
 	if next >= len(keys):
 		print next
 		break
@@ -51,26 +52,63 @@ while i < 100 :
 	phase(i)
 	g(ti)
 	'''
+
 	'''
 	r_bar_(t_i)
 	N_bar_(t)
 	t_bar_(N)
 	'''
-	# record = record + str(i) +"," + t_bar_(N) + "\n"
-	next = next + 1 
-	if ((l_t_b + n_t_b ) > i* percent  ):
-		i = i + 1
-		r_a.append( [tb, (l_t_a - l_t_b) / (ta - tb)])
-		r_b.append( [tb, (n_t_a - n_t_b) / (ta - tb)])
 
+	next = next + 1 
+	if ((l_t_b + n_t_b ) > i_counter* percent  ):
+		i_counter = i_counter + 1
+
+
+		r_n.append( [tb, (l_t_a - l_t_b) / (ta - tb)])
+		r_e.append( [tb, (n_t_a - n_t_b) / (ta - tb)])
+
+
+		r_n_mean = 0.0 
+		r_e_mean = 0.0 
+
+		r_e_mean_recent = 0.0
+		r_n_mean_recent = 0.0
+
+		for i in r_n:
+			r_n_mean = r_n_mean + i[1]
+		r_n_mean = r_n_mean / float(len(r_n))
+
+
+		for i in r_e:
+			r_e_mean = r_e_mean + i[1]
+		r_e_mean = r_e_mean / float(len(r_e))
+
+
+		sub = r_e[-5:]
+		for i in sub:
+			r_e_mean_recent = r_e_mean_recent + i[1]
+		r_e_mean_recent =r_e_mean_recent / float(len(sub))
+
+		sub = r_n[-5:]
+		for i in sub:
+			r_n_mean_recent = r_n_mean_recent + i[1]
+		r_n_mean_recent =r_n_mean_recent / float(len(sub))
+
+		mean.append([tb, r_n_mean , r_e_mean , r_n_mean + r_e_mean])
+		mean_recent.append([tb, r_n_mean_recent , r_e_mean_recent , r_e_mean_recent + r_n_mean_recent])
+		
+		#post execution
 		ta = tb
 		l_t_a = l_t_b
 		n_t_a = n_t_b
 
+# output 
 stri = ""
-for i in range(0, len(r_a)):
-	v1, v2 = r_b[i]
-	v3, v4 = r_a[i]
-	stri = stri + str(v1 / 60 ) + "," + str(v2) + "," + str(v3 /60)+ ","+ str(v4)+  "," + str(v1/60) + "," + str(v2 + v4)+ "\n"
+for i in range(0, len(r_e)):
+	v1, v2 = r_e[i]
+	v3, v4 = r_n[i]
+	stri = stri + str(v1/60.0) + "," + str(v2) + "," + str(v3/60.0)+ ","+ str(v4)+  "," + str(v1/60.0) + "," + str(v2+v4)+ "\n"
 
 open("out_percentage.csv","w+").write(stri)
+
+print mean , "\n",mean_recent
